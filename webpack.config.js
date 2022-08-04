fea0daa684088e4c9092f800761d49c57cf84644
Ryan.Chao <ryanchao1976@gmail.com>
2022-08-04 18:42:29 +0800

    add 1st webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[hash].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[hash].css',
+    }),
+  ],
+};
