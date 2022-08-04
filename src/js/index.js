5abf9e403dd679973fae5a0a0386becaede96cce
Ryan.Chao <ryanchao1976@gmail.com>
2022-08-04 18:59:06 +0800

    add HtmlWebpackPlugin & CleanWebpackPlugin

---
+++ src/js/index.js
@@ -0,0 +1,3 @@
+import './css/a.css'; // 使用 ESM 方式引入
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,31 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/main.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[hash].js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[hash].js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            // 配置 HTML 模板路徑與生成名稱 (第三步)
+            template: 'index.html',
+            filename: 'index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].[hash].css',
+        }),
+        new CleanWebpackPlugin(),
     ],
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'css/[name].[hash].css',
-    }),
-  ],
 };
